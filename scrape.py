0e7f0933adbe61be9950659dabb56fb934ad0c8d
Naveen Anagani <143878171+nav-anaga@users.noreply.github.com>
2024-06-27 01:42:55 +0530

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -6,9 +6,6 @@ reddit = praw.Reddit(client_id=client_id,
                      client_secret=client_secret,
                      user_agent=user_agent)
 
-# Set the username of the user you want to fetch posts from
-
-# Fetch user submissions
 def get_comments(subreddit_name):
   subreddit = reddit.subreddit(subreddit_name)
   return subreddit.stream.comments()
